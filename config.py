7607b4fb720c690527252365aed90f2a932e81fa
Ameeteshp <vinup991@gmail.com>
2024-12-21 21:49:01 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,18 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY @SomsPvt
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7156967034:AAEtACdZ3_6c635nv_SdyNiYgr05WTCI-PE")
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("Ameetesh07")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1604910596")
+
+
+
+
+
+
+
+  
